dc34ba726f8f75d9c099b6e925a8cd279ea09972
hyperspacechain <hyperspecial@naver.com>
2019-11-24 22:35:16 -0800

    context modi

--- test/TestSingAndVerify.js
+++ test/TestSingAndVerify.js
@@ -11,22 +11,22 @@ contract('Verify Test', function (accounts) {
     })
 
     it("Verify test: Signed messages should return signing address", async function () {
-        const messagetoSign = web3.utils.keccak256('messageQQQ');
-        //console.log("messagetoSign(web3.utils.sha3('message')):" + messagetoSign);
+        const messageHash = web3.utils.keccak256('mesjkl,hjk,hsageQfghbcghfgjnhfghjmghfjmgffhjnftyjtbhjgvhfjnfbjfghjbfgbjhfgbhjQQ');
+        //console.log("messageHash(web3.utils.sha3('message')):" + messageHash);
 
         const address = await accounts[0];
         //console.log("address(accounts[0]):" + address);
 
-        var signature = await web3.eth.sign(messagetoSign, address);
-        //console.log("signature(web3.eth.sign(messagetoSign, address)):" + signature);
+        var signature = await web3.eth.sign(messageHash, address);
+        //console.log("signature(web3.eth.sign(messageHash, address)):" + signature);
 
         signatureData = ethUtil.fromRpcSig(signature);
         let v = ethUtil.bufferToHex(signatureData.v);
         let r = ethUtil.bufferToHex(signatureData.r);
         let s = ethUtil.bufferToHex(signatureData.s);
 
-        let recoveredAddress = await validator.recoverAddress(messagetoSign, v, r, s, {from: accounts[0]});
-        console.log('messagetoSign: ' + messagetoSign);
+        let recoveredAddress = await validator.recoverAddress(messageHash, v, r, s, {from: accounts[0]});
+        console.log('messageHash: ' + messageHash);
         console.log('signature: ' + signature);
         console.log('v: ' + v);
         console.log('r: ' + r);
@@ -34,7 +34,7 @@ contract('Verify Test', function (accounts) {
         console.log('address: ' + address + ', returned address: ' + recoveredAddress);
         await assert.equal(recoveredAddress, address);
 
-        let isVerified = await validator.verify(address, messagetoSign, v, r, s, {from: accounts[0]});
+        let isVerified = await validator.verify(address, messageHash, v, r, s, {from: accounts[0]});
         console.log('verification success?: ' + isVerified);
 
     })
