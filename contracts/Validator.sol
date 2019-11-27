2187d00f95ed20688dc82cafc19c8dd6f55e3939
hyperspacechain <hyperspecial@naver.com>
2019-11-27 15:01:28 -0800

    Privatekey_based bool==true

--- contracts/Validator.sol
+++ contracts/Validator.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.24;
  
 contract Validator {
   function recoverAddress(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public constant returns(address) {
-      bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-      bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, msgHash));
+      //bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+      //bytes32 prefixedHash = keccak256(abi.encodePacked(prefix, msgHash));
       return ecrecover(msgHash, v, r, s);
   }
   function verify(address addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) public constant returns(bool) {
